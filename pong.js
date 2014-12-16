ff29b9cc29e204e5b6aed453d061fb45cfb3e886
SwiX <abarbaro@student.42.fr>
2014-12-16 14:41:04 +0100

    Tried to initialize palets - needs debugging

--- pong.js
+++ pong.js
@@ -10,13 +10,37 @@
 //                                                                            //
 // ************************************************************************** //
 
-function pong() {
-  var arcade = document.getElementById('canvas');
-  if (arcade.getContext) {
-    var ctx = arcade.getContext('2d');
+var player = function()
+{
+	var w = 8;
+	var h = 32;
+	var x;
+	var y;
+}
 
-    ctx.fillRect(0,0,arcade.width,arcade.height);
-  }
+player.prototype.draw = function(ctx){
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x, this.y, this.w, this.h);
+}
+
+function pong() {
+	var fgColor = 0x0eff00;
+	var bgColor = 0x000000;
+	var gamestatus = 0;
+	var arcade = document.getElementById('canvas');
+	var p1 = new player;
+	p1.x = arcade.width / 16;
+	p1.y = arcade.height / 2 - p1.h / 2;
+	var p2 = new player;
+	p2.x = 15 * (arcade.width / 16) - p2.w;
+	p2.y = arcade.height / 2 - p2.h / 2;
+
+	if (arcade.getContext) {
+		var ctx = arcade.getContext('2d');
+		ctx.fillRect(0,0,arcade.width,arcade.height);
+	}
+	p1.draw(ctx);
+	p2.draw(ctx);
 }
 
 pong();
